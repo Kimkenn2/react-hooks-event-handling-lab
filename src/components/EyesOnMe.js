a6081d4efb66aab4405511eb392c96ffb6deac93
Kenneth Kim <kkm2059@gmail.com>
2021-11-23 19:00:54 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function EyesFocus() {
+        console.log('Good!')
+    }
+    function EyesBlur() {
+        console.log('Hey! Eyes on me!')
+    }
+        return <button onFocus={EyesFocus} onBlur={EyesBlur}>Eyes on me</button>
+    
+}
+
+export default EyesOnMe
